6fd903376d61e371a6eece8e96e9270cdfb15c5b
Pratik Agrawal <pratikben.1000@gmail.com>
2025-03-05 00:39:41 +0530

    basic layouting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
 export default function Home() {
   return (
-    <div>
-      hello
+    <div className="m-12 flex flex-col items-center space-y-4">
+      <div className="bg-white flex justify-center h-[70vh] w-[80vh] rounded-md">
+      *image
+      </div>
+      <div className="flex flex-col w-[80vh] h-[30vh] bg-white text-center rounded-md">
+        <div>*Heading</div>
+        <div>*Description</div>
+      </div>
     </div>
   );
 }
