f388987dd12d759cc0d1e0fb5a97e2a1e795a92f
Pratik Agrawal <pratikben.1000@gmail.com>
2025-03-06 00:31:53 +0530

    fetched from api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,22 @@
-export default function Home() {
+export default async function Home() {
+const API_URL = `https://newsapi.org/v2/everything?q=english&apiKey=${process.env.NEWS_API}`;
+const response = await fetch(API_URL,{
+  method:"GET",
+  headers:{
+    "content-type":"application/json",
+  }
+});
+
+const data = await response.json()
+console.log(data);
   return (
     <div className="m-12 flex flex-col items-center space-y-4">
       <div className="bg-white flex justify-center h-[70vh] w-[80vh] rounded-md">
-      *image
+      {data.articles[0].urlToImage}
       </div>
       <div className="flex flex-col w-[80vh] h-[30vh] bg-white text-center rounded-md">
-        <div>*Heading</div>
-        <div>*Description</div>
+        <div>{data.articles[0].title} </div>
+        <div>{data.articles[0].description}</div>
       </div>
     </div>
   );
